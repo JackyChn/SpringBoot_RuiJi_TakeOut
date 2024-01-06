f6cd9fbedb8c393100e3acdc5d81438fde3dc447
Jacky <ccc1020048076@gmail.com>
2024-01-06 23:43:05 +1030

    login filter complete

--- src/main/java/com/itcast/reggle/ReggleApplication.java
+++ src/main/java/com/itcast/reggle/ReggleApplication.java
@@ -3,9 +3,11 @@ package com.itcast.reggle;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 
 @Slf4j
 @SpringBootApplication
+@ServletComponentScan // to scan the web filter
 public class ReggleApplication {
 
     public static void main(String[] args) {
---
+++ src/main/java/com/itcast/reggle/filter/CheckLogin.java
@@ -0,0 +1,56 @@
+package com.itcast.reggle.filter;
+
+import com.alibaba.fastjson.JSON;
+import com.itcast.reggle.common.R;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.util.AntPathMatcher;
+
+import javax.servlet.*;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Slf4j
+@WebFilter(filterName = "checkLoginFilter", urlPatterns = "/*")
+public class CheckLogin implements Filter {
+//    path matcher
+    public static final AntPathMatcher PATH_MATCHER = new AntPathMatcher();
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        HttpServletRequest request = (HttpServletRequest) servletRequest;
+        HttpServletResponse response = (HttpServletResponse) servletResponse;
+
+//        1. get request uri
+        String requestURI = request.getRequestURI();
+        String[] urls = new String[] {
+                "/employee/login",
+                "/employee/logout",
+                "/backend/**",
+                "/frontend/**"
+        };
+
+//        2. check if request needed to be filtered, no then pass, if login already, also pass
+        if (checkURL(urls, requestURI) || request.getSession().getAttribute("employee") != null) {
+//            then pass
+            filterChain.doFilter(request, response);
+            return;
+        }
+
+//        3. not login yet, return login result
+        response.getWriter().write(JSON.toJSONString(R.error("NOTLOGIN")));
+    }
+
+    /**
+     * path matcher in list
+     * @param requestURI
+     * @return
+     */
+    public boolean checkURL(String[] urls, String requestURI) {
+        for (String url : urls) {
+            boolean match = PATH_MATCHER.match(url, requestURI);
+            if (match) return true;
+        }
+        return false;
+    }
+}
